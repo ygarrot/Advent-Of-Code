366a6c1d527ac71f2bdfa801b1198b6d70c756ae
ygarrot <garrot.yoan1@gmail.com>
2021-12-09 15:55:57 +0100

    part 2 day 9

--- day_9/src/main.rs
+++ day_9/src/main.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
 const FILENAME: &str = "./resources/day_9.txt";
+// const FILENAME: &str = "./resources/test_2.txt";
 // const FILENAME: &str = "./resources/example.txt";
 
 fn main() -> io::Result<()> {
@@ -11,10 +12,70 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
+fn go_deep(list: &mut Vec<(usize, usize)>,
+    board: Vec<Vec<i64>>,
+    index: (usize, usize)) -> Vec<(usize, usize)>{
+    let COO: Vec<i32> = vec![-1, 0, 1];
+    let (c_x, c_y) = index;
+
+    for y in COO.clone() {
+        let oy = c_y  as i32 + y;
+
+        if oy < 0 || oy >= board.len() as i32 {
+            continue
+        }
+
+        for x in COO.clone() {
+            let ox = c_x  as i32 + x;
+
+            if (x != 0 && y != 0) || (x == 0 && y == 0)
+               || ox < 0 || ox >= board[0].len() as i32 {
+                continue;
+            }
+            if list.contains(&(ox as usize, oy as usize)){
+                continue;
+            }
+
+            if board[oy as usize][ox as usize] < 9 {
+                list.push((ox as usize, oy as usize));
+                go_deep(list, board.clone(), (ox as usize, oy as usize));
+            }
+        }
+    }
+    list.to_vec()
+}
+
+fn find_bassin(board: Vec<Vec<i64>>, lowest: Vec<(usize, usize)>) {
+    let mut test: Vec<Vec<(usize, usize)>> = Vec::new();
+    let mut result: Vec<usize> = Vec::new();
+
+    for low in lowest {
+        let mut list: Vec<(usize, usize)> = vec![low];
+        let mut lst = go_deep(&mut list, board.clone(), low);
+        lst.sort();
+        lst.dedup();
+        result.push(lst.len());
+    }
+
+    result.sort();
+    println!("{:?}", &result[result.len()-3..].iter().fold(1, |total, x| total * x));
+}
+
 fn is_lower(board: Vec<Vec<i64>>, index: (usize, usize)) -> bool {
     let coo: [i32;3] = [-1, 0, 1];
 
     let (c_x, c_y) = index;
+    // let ox = c_x + x;
+    // let oy = c_y + y;
+    // if oy < 0 {
+    //     return is_lower(board, index, x, y + 1)
+    // }
+    // if oy >= board.len() as i32 {
+    //     return is_lower(board, index, x + 1, -1)
+    // }
+    // if (x != 0 && y != 0) || (x == 0 && y == 0){ 
+    // }
+    // true
     for y in coo {
         let oy = c_y  as i32 + y;
 
@@ -35,6 +96,19 @@ fn is_lower(board: Vec<Vec<i64>>, index: (usize, usize)) -> bool {
         }
     }
     true
+}
+
+fn find_lowest_coo(board: Vec<Vec<i64>>) -> Vec<(usize, usize)> {
+    let mut ret: Vec<(usize, usize)> = Vec::new();
+
+    for y in 0..board.len() {
+        for x in 0..board[0].len() {
+            if is_lower(board.to_vec(), (x, y)){
+                ret.push((x, y))
+            }
+        }
+    }
+    ret
 }
 
 fn find_lowest(board: Vec<Vec<i64>>) -> Vec<i64> {
@@ -61,7 +135,13 @@ fn exo1<R: BufRead>(reader: &mut R) -> io::Result<()> {
         total.push(current);
     }
     // println!("{:?}", total[1][1]);
-    let lowest = find_lowest(total);
-    println!("{:?} {:?}", lowest, lowest.iter().sum::<i64>());
+    // let lowest = find_lowest(total.clone());
+    // println!("{:?} {:?}", lowest, lowest.iter().sum::<i64>());
+    // ---------------------------- PART 2 -------------------------------
+    let lowest = find_lowest_coo(total.clone());
+    println!("{:?}", lowest);
+    find_bassin(total, lowest);
+
+
     Ok(())
 }
