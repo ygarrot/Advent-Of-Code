be409b47b8852dae39155c5242966d63493f5382
ygarrot <garrot.yoan1@gmail.com>
2021-12-10 11:44:51 +0100

    tried something but not working

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -23,49 +23,69 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum ValidationCode {
     ERROR,
     VALID
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Token {
     start: char,
-    // expect: char,
+    expect: char,
     found: char,
     i: usize,
     code: ValidationCode
 }
 
-// let BRACK_LIST: HashMap<char, char> = HashMap::from([
-//     ('{', '}'),
-//     ('(', ')'),
-//     ('<', '>'),
-//     ('[', ']'),
-// ]);
-// {([(<{}[<>[]}>{[]{[(<()>
-
 // gerer le cas end bracket before start
-fn match_b(buf: &String, token: Token) -> Token {
+fn match_b(buf: &String, mut token: Token) -> Token {
 
     let cur = buf.as_bytes()[token.i];
-    if cur == token.start as u8 || (!BRACK_LIST.contains_key(&token.start) && cur != BRACK_LIST[&token.start] as u8) {
+    token.found = cur as char;
+
+    // if (token.i >= buf.len()){
+    //     return token;
+    // }
+
+    if cur == token.start as u8
+        || (!BRACK_LIST.contains_key(&token.start) && cur != BRACK_LIST[&token.start] as u8) {
+        token.code = ValidationCode::ERROR;
         return token;
     }
-    if  cur == BRACK_LIST[&token.start] as u8 {
+    if  cur == token.expect as u8 {
         return token;
     }
-    match_b(buf, token)
+    let current_i = token.i;
+    token.i += 1;
+    let found = match_b(buf, token.clone());
+    match found.code {
+        ValidationCode::VALID => {
+            token.i = current_i;
+            let start = buf.as_bytes()[token.i + 1] as char;
+            token.i += 1;
+            // println!("{:?} {:?}", start, token.i);
+            // if !BRACK_LIST.contains_key(&start){
+            //     token.code = ValidationCode::ERROR;
+            //     return token;
+            // }
+            token.start = start;
+            token.expect = BRACK_LIST[&token.start];
+            token = match_b(buf, token)
+        },
+        ValidationCode::ERROR => {}
+    }
+    found
 }
 
 fn exo1<R: BufRead>(reader: &mut R) -> io::Result<()> {
+    let mut i = 0;
     for line in reader.lines() {
         let un = line.unwrap();
         let start = &(un.as_bytes()[0] as char);
-        println!("{:?} {:?}", BRACK_LIST, start);
-        let token =  Token { start: *start, found: BRACK_LIST[&start], code: ValidationCode::VALID, i: 1};
-        println!("{:?}", match_b(&un, token));
+        let token =  Token { start: *start, expect: BRACK_LIST[&start], found: ' ', code: ValidationCode::VALID, i: 1};
+        println!("line:{:?} ===========\n\t {:?}\n\t {:?}", i, un, match_b(&un, token));
+        i+=1;
     }
     Ok(())
 }
